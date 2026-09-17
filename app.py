465c3bb8adb67f1ea65cb1a7ad4b6114d2c956a3
hicenbergromero22-oss <hicenberg.romero22@gmail.com>
2026-09-17 12:17:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def cerebro(m):
             "https://api.groq.com/openai/v1/chat/completions",
             headers={"Authorization": f"Bearer {k}", "Content-Type": "application/json"},
             json={
-                "model":"llama-3.1-8b-instant",
+            "model":"llama-3.3-70b-versatile",
                 "messages":[
                     {"role":"system","content":"Eres Bot Jorge V6, mexicano barrio, buena onda, corto y claro."},
                     {"role":"user","content":m}
